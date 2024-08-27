43229173c06105d20692368c06f2f602ada94159
Luiz Gustavo <150495708+luizgustavosdp@users.noreply.github.com>
2024-08-26 21:22:37 -0300

    Delete EstruturaWhile.py

--- EstruturaWhile.py
+++
@@ -1,8 +0,0 @@
-#Na estrutura 'for' determinamos a quantidades de vezes que será executado o bloco. Na estrutura While o bloco continuará executando enquanto a instrução for verdadeira. 
-#Quando falsa o bloco de comando é ignorado
-x =1;
-while x<=15:
-    print(x)
-    x=x+1
-#Diferente do for, aqui é necessario inicializar a variavel.
-#X é 1. Enquanto x for menor que 15 print x. Para toda vez que o bloco executado incremente +1 em x.
